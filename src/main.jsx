540f7f6a06c8ca3360cabb60c5c019b929632f8a
NicoLe-01 <nicosiahaan9898@gmail.com>
2023-04-10 13:26:55 +0800

    change file structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Navbar from "./NavBar";
-// import Classifier from "./Classifier";
-import ImageClassifier from "./ImageClassifier";
+import Navbar from "./components/NavBar";
+import ImageClassifier from "./components/ImageClassifier";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
